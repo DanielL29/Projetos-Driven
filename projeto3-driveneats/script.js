e26899f0c11d330af3598bd3e5f1ea7d0e320dc6
Daniel <danielnatsufire@gmail.com>
2022-03-31 18:35:57 -0300

    terceira versao

--- projeto3-driveneats/script.js
+++ projeto3-driveneats/script.js
@@ -1,82 +1,117 @@
-let frango = document.getElementById('frango')
-let carne = document.getElementById('carne')
-let ovo = document.getElementById('ovo')
-let pizza = document.getElementById('pizza')
-let hamburguer = document.getElementById('hamburguer')
-let checkmarkFrango = document.getElementById('checkmark-frango')
-let checkmarkCarne = document.getElementById('checkmark-carne')
-let checkmarkOvo = document.getElementById('checkmark-ovo')
-let checkmarkPizza = document.getElementById('checkmark-pizza')
-let checkmarkHamburguer = document.getElementById('checkmark-hamburguer')
+// let frango = document.getElementById('frango')
+// let carne = document.getElementById('carne')
+// let ovo = document.getElementById('ovo')
+// let pizza = document.getElementById('pizza')
+// let hamburguer = document.getElementById('hamburguer')
+// let checkmarkFrango = document.getElementById('checkmark-frango')
+// let checkmarkCarne = document.getElementById('checkmark-carne')
+// let checkmarkOvo = document.getElementById('checkmark-ovo')
+// let checkmarkPizza = document.getElementById('checkmark-pizza')
+// let checkmarkHamburguer = document.getElementById('checkmark-hamburguer')
 
-const green = '6px solid #32B72F'
-const white = '6px solid white'
+// const green = '6px solid #32B72F'
+// const white = '6px solid white'
 
-function changeFoodSelection(id) {
-    frango.style.border = white
-    carne.style.border = white
-    ovo.style.border = white
-    pizza.style.border = white
-    hamburguer.style.border = white
-    checkmarkFrango.style.color = 'white'
-    checkmarkCarne.style.color = 'white'
-    checkmarkOvo.style.color = 'white'
-    checkmarkPizza.style.color = 'white'
-    checkmarkHamburguer.style.color = 'white'
+// function changeFoodSelection(id) {
+//     frango.style.border = white
+//     carne.style.border = white
+//     ovo.style.border = white
+//     pizza.style.border = white
+//     hamburguer.style.border = white
+//     checkmarkFrango.style.color = 'white'
+//     checkmarkCarne.style.color = 'white'
+//     checkmarkOvo.style.color = 'white'
+//     checkmarkPizza.style.color = 'white'
+//     checkmarkHamburguer.style.color = 'white'
 
-    console.log(frango.childNodes[0].nextSibling.childNodes[3].childNodes[1].innerHTML) // nome
-    console.log(frango.childNodes[0].nextSibling.childNodes[3].childNodes[5].childNodes[1].innerHTML) // preço
+//     console.log(frango.childNodes[0].nextSibling.childNodes[3].childNodes[1].innerHTML) // nome
+//     console.log(frango.childNodes[0].nextSibling.childNodes[3].childNodes[5].childNodes[1].innerHTML) // preço
 
-    if(id === 'frango') {
-        if(frango.style.border === white) {
-            frango.style.border = green
-            checkmarkFrango.style.color = '#32B72F'
-        } else {
-            frango.style.border = white
-            checkmarkFrango.style.color = 'white'
+//     if(id === 'frango') {
+//         if(frango.style.border === white) {
+//             frango.style.border = green
+//             checkmarkFrango.style.color = '#32B72F'
+//         } else {
+//             frango.style.border = white
+//             checkmarkFrango.style.color = 'white'
+//         }
+//     } else ''
+//     if(id === 'carne') {
+//         if(carne.style.border === white) {
+//             carne.style.border = green
+//             checkmarkCarne.style.color = '#32B72F'
+//         } else {
+//             carne.style.border = white
+//             checkmarkCarne.style.color = 'white'
+//         }
+//     } else ''
+//     if(id === 'ovo') {
+//         if(ovo.style.border === white) {
+//             ovo.style.border = green
+//             checkmarkOvo.style.color = '#32B72F'
+//         } else {
+//             ovo.style.border = white
+//             checkmarkOvo.style.color = 'white'
+//         }
+//     } else ''
+//     if(id === 'pizza') {
+//         if(pizza.style.border === white) {
+//             pizza.style.border = green
+//             checkmarkPizza.style.color = '#32B72F'
+//         } else {
+//             pizza.style.border = white
+//             checkmarkPizza.style.color = 'white'
+//         }
+//     } else ''
+//     if(id === 'hamburguer') {
+//         if(hamburguer.style.border === white) {
+//             hamburguer.style.border = green
+//             checkmarkHamburguer.style.color = '#32B72F'
+//         } else {
+//             hamburguer.style.border = white
+//             checkmarkHamburguer.style.color = 'white'
+//         }
+//     } else ''
+// }
+
+//     // id === 'frango' ? frango.style.border === white ? frango.style.border = green : frango.style.border = white : ''
+//     // id === 'carne' ? carne.style.border === white ? carne.style.border = green : carne.style.border = white : ''
+//     // id === 'ovo' ? ovo.style.border === white ? ovo.style.border = green : ovo.style.border = white : ''
+//     // id === 'pizza' ? pizza.style.border === white ? pizza.style.border = green : pizza.style.border = white : ''
+//     // id === 'hamburguer' ? hamburguer.style.border === white ? hamburguer.style.border = green : hamburguer.style.border = white : ''
+
+const foodBox = document.querySelectorAll(".food-box");
+const foodSection = document.querySelectorAll("main div");
+const endOrder = document.querySelector("footer div");
+
+foodBox.forEach((el) => {
+  const checkPath = (item) =>
+    item.children[0].children[1].children[2].children[1];
+  const parentPath = el.parentElement.parentElement.parentElement;
+
+  el.onclick = () => {
+    for (let child of el.parentElement.children) {
+      if (child.classList.contains("selected")) {
+        child.classList.remove("selected");
+        checkPath(child).classList.remove("icon-selected");
+      }
+    }
+    el.classList.add("selected");
+    checkPath(el).classList.add("icon-selected");
+    parentPath.classList.add("checked");
+
+    let count = 0;
+    foodSection.forEach((item) => {
+      if (item.classList.contains("checked") === true) {
+        count++;
+        console.log(count);
+        if (count === item.parentElement.children.length) {
+          endOrder.firstElementChild.style.display = "none";
+          endOrder.lastElementChild.style.display = "initial";
         }
-    } else ''
-    if(id === 'carne') {
-        if(carne.style.border === white) {
-            carne.style.border = green
-            checkmarkCarne.style.color = '#32B72F'
-        } else {
-            carne.style.border = white
-            checkmarkCarne.style.color = 'white'
-        }
-    } else ''
-    if(id === 'ovo') {
-        if(ovo.style.border === white) {
-            ovo.style.border = green
-            checkmarkOvo.style.color = '#32B72F'
-        } else {
-            ovo.style.border = white
-            checkmarkOvo.style.color = 'white'
-        }
-    } else ''
-    if(id === 'pizza') {
-        if(pizza.style.border === white) {
-            pizza.style.border = green
-            checkmarkPizza.style.color = '#32B72F'
-        } else {
-            pizza.style.border = white
-            checkmarkPizza.style.color = 'white'
-        }
-    } else ''
-    if(id === 'hamburguer') {
-        if(hamburguer.style.border === white) {
-            hamburguer.style.border = green
-            checkmarkHamburguer.style.color = '#32B72F'
-        } else {
-            hamburguer.style.border = white
-            checkmarkHamburguer.style.color = 'white'
-        }
-    } else ''
-}
+      }
+    });
+  };
+});
 
-    // id === 'frango' ? frango.style.border === white ? frango.style.border = green : frango.style.border = white : ''
-    // id === 'carne' ? carne.style.border === white ? carne.style.border = green : carne.style.border = white : ''
-    // id === 'ovo' ? ovo.style.border === white ? ovo.style.border = green : ovo.style.border = white : ''
-    // id === 'pizza' ? pizza.style.border === white ? pizza.style.border = green : pizza.style.border = white : ''
-    // id === 'hamburguer' ? hamburguer.style.border === white ? hamburguer.style.border = green : hamburguer.style.border = white : ''
-
+endOrder.lastElementChild.onclick = () => {};
